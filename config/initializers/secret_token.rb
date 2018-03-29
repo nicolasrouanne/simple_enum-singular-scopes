cfb58563077ce5cfd4070e38027b28e1a584dc13
Nicolas Rouanne <nico.rouanne@gmail.com>
2018-03-29 16:55:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleEnumSingularScopes::Application.config.secret_key_base = '97c493f1c5f7d72c9f64ed2323cf01c2d42ae2a8689381a9553210ad6b672d0d1eb120096e8e896a51d131358ddc9a9c24a02e05a9a9dec1affaa8e02f724082'
