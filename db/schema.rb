8ada7c308fa4d55ca94acfb44b5b0cba1c8ec7bc
Nicolas Rouanne <nico.rouanne@gmail.com>
2018-03-29 17:12:08 +0200

    feat: generate model Season

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180329151148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "seasons", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
